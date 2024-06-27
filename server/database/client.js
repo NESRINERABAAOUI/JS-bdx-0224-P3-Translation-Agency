dd69260afde7c62d1ec86fa1b9c58ed1da645b94
NESRINERABAAOUI <161383698+NESRINERABAAOUI@users.noreply.github.com>
2024-06-27 23:13:08 +0200

    modules&schemaSQl

--- server/database/client.js
+++ server/database/client.js
@@ -5,11 +5,11 @@ const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 const mysql = require("mysql2/promise");
 
 const client = mysql.createPool({
-  host: DB_HOST,
-  port: DB_PORT,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
 });
 
 client.checkConnection = () => {
